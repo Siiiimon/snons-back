d2100509dbeb2f5d54366f6586d76686a6e157a6
Simon Cornelius <simonjoecornelius@gmail.com>
2022-05-17 03:19:23 +0200

    Added second auth step

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+//const cuid = require('cuid')
+const fetch = require('node-fetch')
 const port = 8080
 
 app.use(cors())
+
+let db = {}
 
 app.get('/auth/setup', (req, res) => {
 	res.send(process.env.CLIENT_KEY)
@@ -11,8 +15,30 @@ app.get('/auth/setup', (req, res) => {
 
 app.get('/auth/redirect', (req, res) => {
 	console.log(`got hit with ${req.query.code} from state: ${req.query.state}`)
+	//const id = cuid()
+	//db[id] = true
+	
+	const auth = Buffer.from(process.env.CLIENT_KEY + ":" + process.env.CLIENT_SECRET).toString('base64')
+	const params = new URLSearchParams()
+	params.append("grant_type", "authorization_code")
+	params.append("code", req.query.code)
+	//params.append("redirect_uri", "https%3A%2F%2Fsnons-back-production.up.railway.app%2Faccess%2Fredirect%2F"+id)
+	params.append("redirect_uri", "https%3A%2F%2Fsnons-back-production.up.railway.app%2Faccess%2Fredirect")
+	fetch("https://api.sonos.com/login/v3/oauth/access", {
+		method: 'POST',
+		body: params,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+			'Authorization': 'Basic ' + auth
+		}
+	}).then(res => res.json()).then(data => console.log("fetch got response: ", data))
 	res.send("back so soon?")
 })
+
+app.get("/access/redirect", (req, res) => {
+	res.send("Ok.")
+})
+
 
 
 app.listen(port, () => {

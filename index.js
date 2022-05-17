da45dad692b93abb99debee8dc028dd799a1be74
Simon Cornelius <simonjoecornelius@gmail.com>
2022-05-17 02:38:07 +0200

    fixed auth routes

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const port = 8080
 
 app.use(cors())
 
-app.route('/auth')
-	.get('/setup', (req, res) => {
-		res.send(process.env.CLIENT_KEY)
-	})
+app.get('/auth/setup', (req, res) => {
+	res.send(process.env.CLIENT_KEY)
+})
 
-	.get('/redirect', (req, res) => {
-		console.log(`got hit with ${req.query.code} from state: ${req.query.state}`)
-		res.send("back so soon?")
-	})
+app.get('/auth/redirect', (req, res) => {
+	console.log(`got hit with ${req.query.code} from state: ${req.query.state}`)
+	res.send("back so soon?")
+})
 
 
 app.listen(port, () => {
